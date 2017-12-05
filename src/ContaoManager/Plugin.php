3b17476d6b70010677b3c0e3f37603640604a019
Stephan Gieb <gieby@live.de>
2017-12-05 16:36:31 +0100

    Routing-Fixes

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -11,6 +11,10 @@ namespace yupdesign\CUABundle\ContaoManager;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
 /**
  * @author Stephan Gieb
  */
